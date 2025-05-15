6ec1c0193c0cdbd2fbc30d17886b8684c2f58861
jinsh1210 <jinsh1210@naver.com>
2025-05-15 18:41:11 +0900

    update

--- Project12/Program.cs
+++ Project12/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,12 +12,11 @@ namespace Project12
     {
         public string name;
         public int bYear;
-        public string major;
-        public Student(string name, int bYear, string major)
+        public const string DEP ="컴퓨터시스템과";
+        public Student(string name, int bYear)
         {
             this.name = name;
             this.bYear = bYear;
-            this.major = major;
         }
     }
 
@@ -24,13 +24,17 @@ namespace Project12
     {
         static void Main(string[] args)
         {
-            Student s1 = new Student("홍길동", 2006, "컴퓨터시스템과");
-            Student s2 = new Student("홍길자", 2005, "컴퓨터시스템과");
-            Student s3 = new Student("홍길순", 2004, "컴퓨터시스템과");
+            Student s1 = new Student("홍길동", 2006);
+            Student s2 = new Student("홍길자", 2005);
+            Student s3 = new Student("홍길순", 2004);
 
             Console.WriteLine(s1.name + " 학생의 출생년도는 " + s1.bYear);
             Console.WriteLine(s2.name + " 학생의 출생년도는 " + s2.bYear);
             Console.WriteLine(s3.name + " 학생의 출생년도는 " + s3.bYear);
+
+            Console.WriteLine(s1.name + " 학생의 학과는 " + Student.DEP);
+            Console.WriteLine(s2.name + " 학생의 학과는 " + Student.DEP);
+
         }
     }
 }
