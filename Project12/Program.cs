5d86efba63af7f4bf9665a8e58ab10c94e76ec6d
jinsh1210 <jinsh1210@naver.com>
2025-05-15 18:47:46 +0900

    update

--- Project12/Program.cs
+++ Project12/Program.cs
@@ -12,11 +12,12 @@ namespace Project12
     {
         public string name;
         public int bYear;
-        public const string DEP ="컴퓨터시스템과";
-        public Student(string name, int bYear)
+        public readonly string DEP = "컴퓨터시스템과";
+        public Student(string name, int bYear, string DEP)
         {
             this.name = name;
             this.bYear = bYear;
+            this.DEP = DEP;
         }
     }
 
@@ -24,16 +25,17 @@ namespace Project12
     {
         static void Main(string[] args)
         {
-            Student s1 = new Student("홍길동", 2006);
-            Student s2 = new Student("홍길자", 2005);
-            Student s3 = new Student("홍길순", 2004);
+            Student s1 = new Student("홍길동", 2006, "컴퓨터시스템과");
+            Student s2 = new Student("홍길자", 2005, "실내건축학과");
+            Student s3 = new Student("홍길순", 2004, "컴퓨터정보과");
 
             Console.WriteLine(s1.name + " 학생의 출생년도는 " + s1.bYear);
             Console.WriteLine(s2.name + " 학생의 출생년도는 " + s2.bYear);
             Console.WriteLine(s3.name + " 학생의 출생년도는 " + s3.bYear);
-
-            Console.WriteLine(s1.name + " 학생의 학과는 " + Student.DEP);
-            Console.WriteLine(s2.name + " 학생의 학과는 " + Student.DEP);
+            Console.WriteLine(s1.name + " 학생의 학과는 " + s1.DEP);
+            //Student.DEP = "실내건축학과";
+            //s1.DEP = "실내건축학과"; // readonly 속성은 생성자에서만 초기화 가능
+            Console.WriteLine(s2.name + " 학생의 학과는 " + s2.DEP);
 
         }
     }
