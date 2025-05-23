388b3019441820da5af1102c3e373662d744b4a4
jinsh <jinsh@naver.com>
2025-05-23 17:40:44 +0900

    update

--- Project11/Program.cs
+++ Project11/Program.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Project11
 {
-    class Test
+    /*class Test
     {
         public int Power(int x)
         {
@@ -78,9 +78,39 @@ namespace Project11
             Console.WriteLine("상품번호 " + t4.id + " : " + t4.name + "의 가격 : " + t4.price + "원");
             Console.WriteLine("오늘 총 입고된 상품의 갯수는 " + Test.count + "개 입니다.");
 
-            
 
 
+
+
+        }
+    }*/
+    public class Student
+    {
+        public string name;
+        public int bYear;
+        public readonly string DEP = "컴퓨터시스템과";
+        public Student(string name, int bYear, string DEP)
+        {
+            this.name = name;
+            this.bYear = bYear;
+            this.DEP = DEP;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student("홍길동", 2006, "컴퓨터시스템과");
+            Student s2 = new Student("홍길자", 2005, "실내건축학과");
+            Student s3 = new Student("홍길순", 2004, "컴퓨터정보과");
+
+            Console.WriteLine(s1.name + " 학생의 출생년도는 " + s1.bYear);
+            Console.WriteLine(s2.name + " 학생의 출생년도는 " + s2.bYear);
+            Console.WriteLine(s3.name + " 학생의 출생년도는 " + s3.bYear);
+            Console.WriteLine(s1.name + " 학생의 학과는 " + s1.DEP);
+            //Student.DEP = "실내건축학과";
+            //s1.DEP = "실내건축학과"; // readonly 속성은 생성자에서만 초기화 가능
+            Console.WriteLine(s2.name + " 학생의 학과는 " + s2.DEP);
 
         }
     }
--- Project12/Program.cs
+++ Project12/Program.cs
@@ -1,41 +1,153 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
-using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Project12
 {
-
-    public class Student
+    /*class Box
     {
-        public string name;
-        public int bYear;
-        public readonly string DEP = "컴퓨터시스템과";
-        public Student(string name, int bYear, string DEP)
+        private int width;
+
+        public int Width
         {
-            this.name = name;
-            this.bYear = bYear;
-            this.DEP = DEP;
+            get { return width; }
+            set
+            {
+                if (value > 0) this.width = value;
+                else Console.WriteLine("양수 값을 입력해");
+            }
+        }
+        private int height;
+        public int Height
+        {
+            get { return height; }
+            set
+            {
+                if (value > 0) this.width = value;
+                else Console.WriteLine("양수 값을 입력해");
+            }
+        }
+
+        public Box(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public int Area()
+        {
+            return this.width * this.height;
         }
     }
 
-    class Program
+    class Circle
+    {
+        private int radius;
+        public int Radius
+        {
+            get { return radius; }
+            set
+            {
+                if (value > 0) this.radius = value;
+                else Console.WriteLine("양수 값을 입력해");
+            }
+        }
+
+        public Circle(int radius)
+        {
+            this.radius = radius;
+        }
+
+        public double Area()
+        {
+            return radius * radius * Math.PI;
+        }*/
+
+    class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("냠냠 먹어요");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine("쿨쿨 자요");
+        }
+    }
+    class Dog : Animal
+    {
+        public string Color { get; set; }
+        public void Bark() { Console.WriteLine("왈왈 짖어요"); }
+    }
+
+    class Cat : Animal
+    {
+        public void Meow() { Console.WriteLine("냥냥 웁니다"); }
+    }
+
+    internal class Program
     {
         static void Main(string[] args)
         {
-            Student s1 = new Student("홍길동", 2006, "컴퓨터시스템과");
-            Student s2 = new Student("홍길자", 2005, "실내건축학과");
-            Student s3 = new Student("홍길순", 2004, "컴퓨터정보과");
+            Console.OutputEncoding = System.Text.Encoding.UTF8; // C#
+            /*Box box = new Box(10, 10);
+            Console.WriteLine("현재 너비는 " + box.Width +
+                " 현재 높이는 " + box.Height);
+            Console.WriteLine("현재 면적은 " + box.Area());
+            box.Width = 20;
+            box.Height = 30;
+            Console.WriteLine("현재 너비는 " + box.Width +
+                " 현재 높이는 " + box.Height);
+            Console.WriteLine("현재 면적은 " + box.Area());*/
 
-            Console.WriteLine(s1.name + " 학생의 출생년도는 " + s1.bYear);
-            Console.WriteLine(s2.name + " 학생의 출생년도는 " + s2.bYear);
-            Console.WriteLine(s3.name + " 학생의 출생년도는 " + s3.bYear);
-            Console.WriteLine(s1.name + " 학생의 학과는 " + s1.DEP);
-            //Student.DEP = "실내건축학과";
-            //s1.DEP = "실내건축학과"; // readonly 속성은 생성자에서만 초기화 가능
-            Console.WriteLine(s2.name + " 학생의 학과는 " + s2.DEP);
+            /*Circle c1 = new Circle(10);
+            Console.WriteLine("현재 반지름 " + c1.Radius);
+            Console.WriteLine("현재 원의 면적 " + c1.Area());
+            c1.radius = 100;
+            Console.WriteLine("현재 반지름 " + c1.Radius);
+            Console.WriteLine("현재 원의 면적 " + c1.Area());*/
+
+            /*Animal a1 = new Animal();
+            a1.Eat();
+            a1.Sleep();
+            //a1.Bark();
+            Dog d1 = new Dog();
+            d1.Eat();
+            d1.Sleep();
+            d1.Bark();
+            Animal a2 = new Dog();
+            a2.Eat();
+            a2.Sleep();
+            //(a2 as Dog).Bark();
+            ((Dog)a2).Bark();*/
+
+            List<Animal> a1 = new List<Animal>()
+            {
+                new Dog(), new Cat(), new Dog(), new Dog()
+            };
+
+            foreach (var item in a1)
+            {
+
+                if (item is Dog)
+                {
+                    Console.WriteLine("강아지는 이런 일을 합니다.");
+                    item.Eat();
+                    item.Sleep();
+                    (item as Dog).Bark();
+                }
+                else if (item is Cat)
+                {
+                    Console.WriteLine("고양이는 이런 일을 합니다.");
+                    item.Eat();
+                    item.Sleep();
+                    ((Cat)item).Meow();
+                }
+            }
 
         }
     }
